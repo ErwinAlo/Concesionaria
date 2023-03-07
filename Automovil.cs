aca3808a0e2601ae963fc009b026f174ce777f66
ErwinAlo <ealo2347@gmail.com>
2023-03-07 15:38:35 -0600

    Clase Automovil

---
+++ Automovil.cs
@@ -0,0 +1,15 @@
+  namespace Consecionaria;
+   
+    public class Automovil{
+      public string Placa;
+      public string Marca;
+      public string Modelo;
+
+      public int Km = 0;
+      public double Precio = 0;
+
+      public override string ToString(){
+        return $"Id: {Placa}"+ $"Marca: {Marca}" + $"Modelo: {Modelo}"+ "kM: {Km} "+ $"Precio {Precio}";
+      }
+
+    }

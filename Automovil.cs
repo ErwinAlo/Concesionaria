068adc886ed14ee322becab64002bc0cb45a023a
ErwinAlo <ealo2347@gmail.com>
2023-03-07 23:42:53 -0600

    Clase Concesionarial

--- Automovil.cs
+++ Automovil.cs
@@ -1,15 +1,15 @@
-  namespace Consecionaria;
+  namespace Concesionaria;
    
-    public class Automovil{
-      public string Placa;
-      public string Marca;
-      public string Modelo;
+      public class Automovil{
+        public string Placa;
+        public string Marca;
+        public string Modelo;
 
-      public int Km = 0;
-      public double Precio = 0;
+        public int Km = 0;
+        public double Precio = 0;
 
-      public override string ToString(){
-        return $"Id: {Placa}"+ $"Marca: {Marca}" + $"Modelo: {Modelo}"+ "kM: {Km} "+ $"Precio {Precio}";
-      }
+        public override string ToString(){
+         return $"Id: {Placa}"+ $"Marca: {Marca}" + $"Modelo: {Modelo}"+ "kM: {Km} "+ $"Precio: {Precio}";
+       }
 
     }
---
+++ concesionaria.cs
@@ -0,0 +1,82 @@
+  namespace Concesionaria;
+  
+      public class concesionaria{
+
+       public List<Automovil> lista;
+       public int LimiteAuto;
+       public int NumActual= 0;
+
+       public concesionaria(int LimiteAuto){
+
+          lista = new List<Automovil>();
+          this.LimiteAuto = LimiteAuto;
+       }
+
+       public void AgregarAuto( Automovil Auto){
+
+           if( NumActual < LimiteAuto ){
+
+              lista.Add(Auto);
+              NumActual ++;
+              
+           }else{
+
+              Console.WriteLine(" No hay espacio ");
+
+           }
+
+       }
+       public void MostrarAuto(Automovil Auto){
+         
+            if( Auto != null ){
+
+                foreach(Automovil item in lista){
+
+                      if( item == Auto ){
+                            Console.WriteLine(item);
+                            return;
+                      }
+                   
+                }
+                Console.WriteLine("No se encontro el auto");
+
+            }
+
+       }
+       public void EliminarAuto(Automovil Auto){
+
+         if( NumActual != 0 ){
+
+                foreach(Automovil item in lista){
+
+                      if(item == Auto){
+                            lista.Remove(item);
+                            NumActual --;  
+                            return;  
+                      }
+
+                }
+
+            }else{
+               Console.WriteLine("Esta vacio");
+            }
+
+       }
+       public void MostrarAutos(Automovil Auto){
+
+         foreach(Automovil item in lista){
+            Console.WriteLine(item);
+         }
+
+       }
+       public void VaciarConcesionaria(Automovil Auto){
+
+            foreach(Automovil item in lista){
+            lista.Clear();
+            Console.WriteLine("No hay autos");
+
+         }
+
+       }
+
+      } 
